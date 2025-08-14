eea3a4b79e35876d7c15d037a3cb94320dbddc1e
Jame Hardin <jamehardin14@gmail.com>
2025-08-14 08:09:40 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  experimental: {
+    // This is to allow cross-origin requests from the development environment.
+    allowedDevOrigins: ['**'],
+  },
 };
 
 export default nextConfig;
